c0aac5d8faa3d84ceb6a43edfdb017c0b8415d19
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2025-12-04 19:18:51 -0600

    cleanup

--- src/PasswordProtect.jsx
+++ src/PasswordProtect.jsx
@@ -60,6 +60,7 @@ function PasswordProtect({ children }) {
               placeholder="Enter password"
               className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-green-500 focus:border-transparent outline-none transition"
               autoFocus
+              autoComplete="current-password"
             />
             <button
               type="button"
